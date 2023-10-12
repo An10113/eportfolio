331df40108cd77d7c571e240acd047d692ffdcb9
An10113 <antranthanh1011@gmail.com>
2023-10-13 02:17:25 +0700

    done

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,18 +1,14 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
+import { Modal } from "@mui/material";
 
 export default function NavBar() {
-  function openMenu() {
-    document.body.classList += "menu--open";
-  }
-  function closeMenu() {
-    document.body.classList.remove("menu--open");
-  }
+  const [open, setOpen] = useState(false);
 
   return (
-    <nav className="p-5 top-0 flex items-start justify-between max-w-7xl mx-auto z-10">
+    <nav className="p-5 top-0 flex items-start justify-between max-w-7xl mx-auto">
       <img src="/assets/letter-a.png" className="w-16 h-16" />
-      <div className=" items-center md:flex hidden ">
+      <div className=" items-center md:flex hidden z-10">
         <a
           href="#about"
           className="hover:scale-110 transition duration-200 ease p-3"
@@ -46,23 +42,32 @@ export default function NavBar() {
           </div>
         </a>
       </div>
-      {/* <button
+      <button
         className="md:hidden flex items-center w-12 h-12"
-        onClick={openMenu}
+        onClick={() => setOpen(!open)}
       >
         <FaBars size={30} color="#337357" />
       </button>
-      <div className="menu__backdrop">
-        <button className="btn__menu btn__menu--close" onClick={closeMenu}>
-          <FaTimes size={30} color="#337357" />
-        </button>
-        <ul className=" flex text-2xl items-center justify-around flex-col text-white">
-          <a className="p-8" href="#about">About</a>
-          <a className="p-8" href="#techstack">Tech</a>
-          <a className="p-8" href="#project">Project</a>
-          <a className="p-8" href="#contact">Contact</a>
-        </ul>
-      </div> */}
+      <Modal
+        open={open}
+        onClose={() => setOpen(false)}
+        className="bg-black items-center flex justify-center"
+      >
+        <div className="text-white text-2xl items-center flex flex-col">
+          <a className="p-6" onClick={()=> setOpen(false)} href="#about">
+            <div>About</div>
+          </a>
+          <a className="p-6" onClick={()=> setOpen(false)} href="#techstack">
+            <div>Tech</div>
+          </a>
+          <a className="p-6" onClick={()=> setOpen(false)} href="#project">
+            <div>Project</div>
+          </a>
+          <a className="p-6" onClick={()=> setOpen(false)} href="#contact">
+            <div>Contact</div>
+          </a>
+        </div>
+      </Modal>
     </nav>
   );
 }
